e589282f2476c3813330f037ae683f8dd69d31fd
Jonny Molen <jonnymolen@gmail.com>
2026-09-15 18:59:14 -0700

    javascript foundation
    implemented array of symbols and the numbers

---
+++ script.js
@@ -0,0 +1,10 @@
+// implement an array of user choices with buttons
+const symbols = ['+', '-', '/', '*', '.', '=', 'del', 'clear'];
+
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// add action buttons that push user input into the display page
+    //user cannot do multiple symbols in a row
+
+
+// add hover color changer to the buttons
